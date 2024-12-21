415886a9dfd125c1dda83271447e743076783754
irenerukk <irenecasenavecardona2003@gmail.com>
2024-12-21 19:31:33 +0100

    cambio final por hoy

--- script.js
+++ script.js
@@ -6,15 +6,3 @@ document.addEventListener("mousemove", (event) => {
   customCursor.style.left = `${clientX}px`;
   customCursor.style.top = `${clientY}px`;
 });
-
-// Deshabilitar arrastre para todas las imágenes en la página
-document.querySelectorAll("img").forEach((img) => {
-  img.addEventListener("dragstart", (event) => {
-    event.preventDefault(); // Prevenir el evento dragstart
-  });
-});
-document.querySelectorAll("img").forEach((img) => {
-  img.addEventListener("contextmenu", (event) => {
-    event.preventDefault(); // Prevenir el clic derecho
-  });
-});
