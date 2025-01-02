53b83715f3809d45d91c4184121e553c2d0535be
irenerukk <irenecasenavecardona2003@gmail.com>
2025-01-02 02:25:27 +0100

    reestructuración de css y html

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ document.addEventListener("mousemove", (event) => {
   customCursor.style.left = `${clientX}px`;
   customCursor.style.top = `${clientY}px`;
 });
+document.addEventListener("DOMContentLoaded", () => {
+  const menuBtn = document.querySelector(".menu-btn");
+  const menuContent = document.querySelector(".menu-content");
+
+  // Alternar la visibilidad del menú al hacer clic en el botón
+  menuBtn.addEventListener("click", () => {
+    menuContent.style.display =
+      menuContent.style.display === "block" ? "none" : "block";
+  });
+
+  // Cerrar el menú si se hace clic fuera de él
+  window.addEventListener("click", (e) => {
+    if (!menuBtn.contains(e.target) && !menuContent.contains(e.target)) {
+      menuContent.style.display = "none";
+    }
+  });
+});
